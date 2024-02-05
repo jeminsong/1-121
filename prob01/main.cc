1e5133b5d49a39b5c0c8c3ed5c5d79286c9aa737
jeminsong <jeminsong0119@csu.fullerton.edu>
2024-02-04 22:03:25 -0800

    finished lab01 prob01

--- prob01/main.cc
+++ prob01/main.cc
@@ -2,6 +2,27 @@
 #include <iostream>
 
 int main() {
-  // TODO: Create a program that computes the restaurant bill.
+  double tax = 0.075;
+  double tip = 0.0;
+  double subtotal = 0.0;
+  std::cout << "Please input meal cost: ";
+  std::cin >> subtotal;
+  std::cout << "Please input tip percentage: ";
+  std::cin >> tip;
+  std::cout << "\n"
+            << "Restaurant Bill"
+            << "\n";
+  std::cout << "===================="
+            << "\n";
+  std::cout << std::fixed << std::setprecision(2) << "Subtotal: $" << subtotal
+            << "\n";
+  std::cout << std::fixed << std::setprecision(2) << "Taxes: $"
+            << tax * subtotal << "\n";
+  std::cout << std::fixed << std::setprecision(2) << "Tip: $"
+            << (tip / 100.0) * subtotal << "\n";
+  std::cout << "===================="
+            << "\n";
+  std::cout << std::fixed << std::setprecision(2) << "Total: $"
+            << subtotal + (tax * subtotal) + ((tip / 100) * subtotal) << "\n";
   return 0;
 }
